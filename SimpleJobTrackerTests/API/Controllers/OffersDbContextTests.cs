93c85e2b5dd4e249215107de1badc897261d2e9f
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2023-05-03 20:52:17 +0200

    Add OffersDbContextTests

---
+++ SimpleJobTrackerTests/API/Controllers/OffersDbContextTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimpleJobTrackerTests.API.Controllers
+{
+    public class OffersDbContextTests
+    {
+        [Fact]
+        public void OffersDbContextExists()
+        {
+            var mockOptions = new DbContextOptionsBuilder<OffersDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var offersDbContext = new OffersDbContext(mockOptions);
+
+            Assert.NotNull(offersDbContext);
+        }
+
+        [Fact]
+        public void JobOffersProperty()
+        {
+            var mockOptions = new DbContextOptionsBuilder<OffersDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var offersDbContext = new OffersDbContext(mockOptions);
+            int id = 1;
+            var jobOffer = new JobOfferModel() { Id = id };
+
+            offersDbContext.JobOffers.Add(jobOffer);
+            offersDbContext.SaveChanges();
+
+            Assert.Equal(jobOffer, offersDbContext.JobOffers.First());
+
+            offersDbContext.JobOffers.Remove(jobOffer);
+            offersDbContext.SaveChanges();
+
+            Assert.Equal(0, offersDbContext.JobOffers.Count());
+        }
+    }
+}
