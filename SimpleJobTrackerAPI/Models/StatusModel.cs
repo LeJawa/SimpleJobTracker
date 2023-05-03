e1861ec1a4e126d10c51553900b9c381a8e8368d
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2023-05-03 17:24:41 +0200

    Create StatusModelTests

--- SimpleJobTrackerAPI/Models/StatusModel.cs
+++ SimpleJobTrackerAPI/Models/StatusModel.cs
@@ -2,5 +2,7 @@
 {
     public class StatusModel
     {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
     }
 }
---
+++ SimpleJobTrackerTests/API/Models/StatusModelTests.cs
@@ -0,0 +1,35 @@
+﻿namespace SimpleJobTrackerTests.API.Models
+{
+    public class StatusModelTests
+    {
+        [Fact]
+        public void StatusModelExists()
+        {
+            StatusModel status = new StatusModel();
+
+            Assert.NotNull(status);
+        }
+
+        [Fact]
+        public void IdProperty()
+        {
+            StatusModel status = new StatusModel();
+            int id = 1;
+
+            status.Id = id;
+
+            Assert.Equal(id, status.Id);
+        }
+
+        [Fact]
+        public void DescriptionProperty()
+        {
+            StatusModel status = new StatusModel();
+            string description = "Description";
+
+            status.Description = description;
+
+            Assert.Equal(description, status.Description);
+        }
+    }
+}
