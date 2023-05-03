7e3d07a6de4a4e73f4cf361a0492ff9371a39868
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2023-05-03 17:24:05 +0200

    Create CompanyModelTests

---
+++ SimpleJobTrackerTests/API/Models/CompanyModelTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleJobTrackerTests.API.Models
+{
+    public class CompanyModelTests
+    {
+        [Fact]
+        public void CompanyModelExists()
+        {
+            CompanyModel company = new CompanyModel();
+
+            Assert.NotNull(company);
+        }
+
+        [Fact]
+        public void IdProperty()
+        {
+            CompanyModel company = new CompanyModel();
+            int id = 1;
+
+            company.Id = id;
+
+            Assert.Equal(id, company.Id);
+        }
+
+        [Fact]
+        public void NameProperty()
+        {
+            CompanyModel company = new CompanyModel();
+            string name = "Name";
+
+            company.Name = name;
+
+            Assert.Equal(name, company.Name);
+        }
+    }
+}
